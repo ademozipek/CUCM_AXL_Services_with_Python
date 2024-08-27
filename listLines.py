708033bb7d8f012a732144df1272bb45dfd780d5
Adem ÖZİPEK <72045831+ademozipek@users.noreply.github.com>
2024-08-27 08:45:42 +0300

    Create listLines.py

---
+++ listLines.py
@@ -0,0 +1,57 @@
+# Dahililerin listelenmesi / Listing lines
+# Adem ÖZİPEK
+
+import logging
+import pprint
+from lxml import etree
+import zeep
+import requests
+import urllib3
+from requests.auth import HTTPBasicAuth
+
+#Değişkenler / Variables
+CUCM_SERVER_IP_ADDRESS = "1.1.1.1"
+CUCM_AXL_USERNAME = "admin" #Kullanıcı AXL erişim yetkisine sahip olmalı / The user must access the AXL service
+CUCM_AXL_PASSWORD = "C!sco123"
+#AXLAPI.wsdl ve AXLSoap.xsd dosyalarının CUCM üzerinden indirilip yolunun belirtilmesi gerekiyor.
+#Defining path AXLAPI.wsdl and AXLSOAP.xsd files downloaded from CUCM
+AXL_WSDL_FILE = "AXLAPI.wsdl"
+
+#Oturum başlatıyoruz. /  Starting session.
+session = requests.Session()
+#SSL Sertifikası kullanmak istemiyorsanız. / Avoid using an SSL certificate.
+session.verify = False
+#Güvensiz istekler uyarısını atlamak için / Ignores Insecure request warning.
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+#Kimlik doğrulama / Authentication
+session.auth = requests.auth.HTTPBasicAuth(CUCM_AXL_USERNAME,CUCM_AXL_PASSWORD)
+#Hata Ayıklama / Debugging
+history = zeep.plugins.HistoryPlugin()
+#Zeep client oluşturuyoruz / Create an instance of the zeep.
+client = zeep.Client(wsdl=AXL_WSDL_FILE, transport=zeep.Transport(session=session), plugins=[history])
+#AXL servisini oluşturuyoruz / Create the AXL service
+service = client.create_service('{http://www.cisco.com/AXLAPIService/}AXLAPIBinding',
+                                f'https://' + CUCM_SERVER_IP_ADDRESS + ':8443/axl/')
+
+#Arama kriterleri / Search criteria
+search_criteria = {
+    #'pattern':'123%'
+    'description':'A%' #Sondaki % işareti ile A harfiyle başlayan sonuçları filtreliyoruz. / % for starts with
+}
+
+#Sonuçlarda gösterilecek alanlar / Returned tags
+returned_tags = {
+    'pattern' : '%', #Line numarası / Line number
+    'description' : '%'
+}
+
+#Info seviyesindeki logları görüntülemek için / Setting log severity
+logging.getLogger().setLevel(logging.INFO)
+
+#listLine AXL metodunu çalıştırarak sorgulama yapıyoruz / Executing listLine AXL method
+response = service.listLine(searchCriteria=search_criteria, returnedTags=returned_tags)
+
+#Sonuçlar / Results
+logging.info('İstek/Query:\n %s', etree.tostring(history.last_sent["envelope"], encoding="unicode", pretty_print=True))
+logging.info('Cevap/Response:\n %s',etree.tostring(history.last_received["envelope"], encoding='unicode', pretty_print=True))
+logging.info('Biçimlendirilmiş/Parsed:\n %s', pprint.pformat(response))
